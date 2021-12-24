c9cc8ff2499b2016bbfd7c3e7a8ef8c33bb9e6e9
Mazen <mazenaboelanin@gmail.com>
2021-12-24 16:32:02 +0200

    created bootcamp model

---
+++ models/Bootcamp.js
@@ -0,0 +1,105 @@
+const {Schema} = require('mongoose');
+
+const BootcampSchema = new Schema({
+
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim:true.value,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    description:{
+        type: String,
+        required: [true, 'Please add a description '],
+        maxlength: [500, 'description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 'Please use a vailed URL with Http or Https']
+    },
+    phone:{
+        type: String,
+        maxlength: [20, ' Phone Number can not be loger than 20 characters']
+    },
+    email: {
+        type: String,
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a vailed email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please Add an address']
+    },
+
+    location:{
+        //GeoJSON Point
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+          },
+          coordinates: {
+            type: [Number],
+            required: true,
+            index:'2dsphere'
+          },
+        formattedAddress:String,
+        street:String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
+    },
+    careers:{
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business', 
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min : [1, 'Rating must be at least 1'],
+        max : [10,' Rating must can not be more than 10'],
+    },
+    averageCost: Number,
+    // name of the photo
+    photo: {
+        type: String,
+        default:'no-photo.jpg'
+    },
+    housing:{
+        type:Boolean,
+        default: false
+    },
+    jobAssistance:{
+        type:Boolean,
+        default: false
+    },
+    jobGuarantee:{
+        type:Boolean,
+        default: false
+    },
+    acceptGi:{
+        type:Boolean,
+        default: false
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    },
+
+});
+
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
